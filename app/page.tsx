91e0eedc2ac3e637ca5dbe34576233ebddecbfd4
Yuttana.H <yhumda@gmail.com>
2025-12-16 16:29:26 +0700

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function MapPage() {
     <div className="relative h-screen w-full">
       <header className="absolute top-0 left-0 right-0 z-20 flex justify-center pointer-events-none">
         <div className="mt-4 px-8 py-2 bg-white/70 backdrop-blur-md rounded-full shadow-md max-w-[50%]">
-          <h1 className="text-xl font-semibold tracking-wide text-neutral-600">
+          <h1 className="text-md text-center md:text-xl text-xl font-semibold tracking-wide text-neutral-600">
             Naxia Early Warning Pole
           </h1>
         </div>
